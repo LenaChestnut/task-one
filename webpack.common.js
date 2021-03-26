3ce8264b1951a101cac54b5370cfcb6d7dceacca
Elena Kashtanova <lenachestnut@gmail.com>
2021-03-26 13:25:14 +0300

    Set up project environment

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: 'vendors',
+                    test: /node_modules/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
+    },
+    plugins: [new ESLintPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name]',
+                },
+            },
+        ],
+    },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '',
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            new TerserPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.pug',
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true,
+                },
+            }),
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+});
