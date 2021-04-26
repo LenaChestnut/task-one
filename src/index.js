01581f8fe87e9f12ec79ee9c5dcae2fd362be9bc
Elena Kashtanova <lenachestnut@gmail.com>
2021-04-26 17:09:37 +0300

    Import all .js files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import 'normalize.css';
 import './styles.scss';
+
+function importAll(requireContext) {
+    requireContext.keys().forEach(requireContext);
+}
+
+importAll(require.context('.', true, /\.js$/));
